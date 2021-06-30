2c64c03159ac1708369ae1bb00576891599a00ac
rcourivaud <r.courivaiud@gmail.com>
2021-06-30 15:43:26 +0200

    add elasticsearch and header handling
    Signed-off-by: rcourivaud <r.courivaiud@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kong_back_front
+
+[<img src="https://konghq.com/">](https://konghq.com/)
--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Header, Request
+from typing import Optional
+import base64
+import json
 app = FastAPI()
 
 
@@ -11,3 +13,11 @@ def read_root():
 @app.get("/api")
 def read_hello():
     return {"Hello": "Api"}
+    
+
+
+@app.get("/api/headers")
+def read_hello(request: Request, x_userinfo: Optional[str] = Header(None, convert_underscores=True), ):
+    print(request["headers"])
+    return {"Headers": json.loads(base64.b64decode(x_userinfo))}
+
